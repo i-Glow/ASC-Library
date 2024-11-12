d2a79500b6fec2bdbdcca965f6e6251386793c4b
Abdelmalek <devmailglow@gmail.com>
2024-11-12 22:04:10 +0100

    implemented add to cart functionality

--- browse/app.js
+++ browse/app.js
@@ -43,6 +43,7 @@ window.onload = async function () {
     const image = document.createElement("img");
     image.src = data.image;
     image.style.width = "200px";
+    image.style.aspectRatio = "3/4";
     image.style.marginBottom = "10px";
 
     const orderButton = document.createElement("button");
@@ -54,7 +55,29 @@ window.onload = async function () {
     orderButton.style.borderRadius = "3px";
     orderButton.style.cursor = "pointer";
     orderButton.onclick = () => {
-      console.log("to be implemented");
+      addBookToCart({ bookId: doc.id, returnDate: "", type: "achat" });
+      alert(`book added to cart: ${data.name}`);
+    };
+
+    const borrowButton = document.createElement("button");
+    borrowButton.textContent = "Borrow (15d)";
+    borrowButton.style.margin = "0px 10px";
+    borrowButton.style.padding = "5px 10px";
+    borrowButton.style.backgroundColor = "#4CAF50";
+    borrowButton.style.color = "white";
+    borrowButton.style.border = "none";
+    borrowButton.style.borderRadius = "3px";
+    borrowButton.style.cursor = "pointer";
+    borrowButton.onclick = () => {
+      const date = new Date();
+      date.setDate(date.getDate() + 15);
+
+      addBookToCart({
+        bookId: doc.id,
+        returnDate: new Date(date).toISOString(),
+        type: "emprunt",
+      });
+      alert(`book added to cart: ${data.name}`);
     };
 
     bookDiv.appendChild(image);
@@ -62,7 +85,24 @@ window.onload = async function () {
     bookDiv.appendChild(author);
     bookDiv.appendChild(price);
     bookDiv.appendChild(orderButton);
+    bookDiv.appendChild(borrowButton);
 
     bookList.appendChild(bookDiv);
   });
 };
+
+function addBookToCart(book) {
+  const cart = getCookie("cart");
+  const bookList = cart ? JSON.parse(cart) : [];
+  bookList.push(book);
+
+  document.cookie = `cart=${JSON.stringify(
+    bookList
+  )}; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/`;
+}
+
+function getCookie(name) {
+  const value = "; " + document.cookie;
+  const parts = value.split("; " + name + "=");
+  if (parts.length === 2) return parts.pop().split(";").shift();
+}
